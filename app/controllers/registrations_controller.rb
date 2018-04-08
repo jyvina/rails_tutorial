3cdbbf0530de0e728c46fe238c0a0db07079d766
kubota masashi <jyvina@gmail.com>
2018-04-08 23:46:52 +0000

    Finish user edit action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,5 +8,9 @@ class RegistrationsController < Devise::RegistrationsController
         def sign_up_params
             params.require(:user).permit(:name, :email, :password, :password_confirmation)
         end
+        
+        def account_update_params
+            params.require(:user).permit(:email, :password, :password_confirmation, :current_passwerd)
+        end
 
 end
